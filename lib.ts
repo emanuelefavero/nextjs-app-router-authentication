9c4bd7ae8d5fc6f03242f99098ee5cdf0d62a13e
emanuelefavero <emafaux89@gmail.com>
2024-04-11 18:16:30 +0200

    add main init code

---
+++ lib.ts
@@ -0,0 +1,33 @@
+import { SignJWT, jwtVerify } from 'jose'
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+const secretKey = 'secret'
+const key = new TextEncoder().encode(secretKey)
+
+export async function encrypt(payload: any) {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('10 sec from now')
+    .sign(key)
+}
+
+export async function decrypt(input: string): Promise<any> {
+  const { payload } = await jwtVerify(input, key, {
+    algorithms: ['HS256'],
+  })
+  return payload
+}
+
+// * Login
+export async function login(formData: FormData) {}
+
+// * Logout
+export async function logout() {}
+
+// * Get Session
+export async function getSession() {}
+
+// * Update Session
+export async function updateSession(request: NextRequest) {}
