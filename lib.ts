8f91b2ee00bf13b93ed182b18c90278aa1e63e2e
emanuelefavero <emafaux89@gmail.com>
2024-04-11 18:38:56 +0200

    add updateSession function

--- lib.ts
+++ lib.ts
@@ -48,4 +48,22 @@ export async function getSession() {
 }
 
 // * Update Session
-export async function updateSession(request: NextRequest) {}
+export async function updateSession(request: NextRequest) {
+  const session = request.cookies.get('session')?.value
+  if (!session) return
+
+  // Refresh the session so it doesn't expire
+  const parsed = await decrypt(session)
+  parsed.expires = new Date(Date.now() + 10 * 1000)
+
+  // Save the session in a cookie
+  const res = NextResponse.next()
+  res.cookies.set({
+    name: 'session',
+    value: await encrypt(parsed),
+    httpOnly: true,
+    expires: parsed.expires,
+  })
+
+  return res
+}
